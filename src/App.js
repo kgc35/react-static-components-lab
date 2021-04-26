1dda66ba6189d95eac1cf390185e5cdcb269458a
kent <kntchn@gmail.com>
2021-04-26 15:40:56 -0400

    commit tree 9afca5c56f4a0b062aff6593eff69fa1dd519749

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import CatComponent from "./CatComponent";
+import GraceHopperQuoteComponent from "./GraceHopperQuoteComponent";
+import Mouse from "./MouseComponent";
 
 class App extends Component {
-	render() {
-		// your code in the return statement below!
-		return (
-			<div className="App">
-				<CatComponent />
-				<GraceHopperQuoteComponent />
-				{/* one more component missing */}
-			</div>
-		);
-	}
+  render() {
+    // your code in the return statement below!
+    return (
+      <div className="App">
+        <CatComponent />
+        <GraceHopperQuoteComponent />
+        <Mouse />
+      </div>
+    );
+  }
 }
 
 export default App;
